c8d94dbbf92d6af8ab1d4d7c6adc242d334d79f1
zerg41 <zerg41@gmail.com>
2023-11-22 19:29:48 +0300

    feat: show how to update arrays and objects

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,8 @@
 <script>
   let count = 0;
+  let numbers = [1, 2, 3, 4];
+
+  let obj = { foo: { bar: 'bar' } };
 
   $: doubled = count * 2;
   $: if (count >= 10) {
@@ -7,8 +10,19 @@
     count = 0;
   }
 
+  $: nextNumber = numbers.length + 1;
+  $: sum = numbers.reduce((total, current) => total + current, 0);
+
   function increment() {
     count++;
+  }
+
+  function addNumber() {
+    numbers = [...numbers, nextNumber];
+  }
+
+  function updateFoo() {
+    obj = { ...obj, foo: { bar: 'baz' } };
   }
 </script>
 
@@ -16,5 +30,13 @@
   Clicked {count}
   {count === 1 ? 'time' : 'times'}
 </button>
-
 <p>{count} doubled is {doubled}</p>
+
+<button on:click={addNumber}>
+  Add Number {nextNumber}
+</button>
+<p>{numbers.join(' + ')} = {sum}</p>
+
+<button on:click={updateFoo}>
+  obj.foo.bar = {obj.foo.bar}
+</button>
