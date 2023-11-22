1381c6bd2bf9d3d8ed7ea722c5b9dd6a0242b9df
zerg41 <zerg41@gmail.com>
2023-11-22 18:40:41 +0300

    feat: add reactivity

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,20 @@
 <script>
-  let string = `this string contains some <strong>HTML!!!</strong`;
+  let count = 0;
+
+  $: doubled = count * 2;
+  $: if (count >= 10) {
+    alert('count is dangerously high!');
+    count = 0;
+  }
+
+  function increment() {
+    count++;
+  }
 </script>
 
-<p>{@html string}</p>
+<button on:click={increment}>
+  Clicked {count}
+  {count === 1 ? 'time' : 'times'}
+</button>
+
+<p>{count} doubled is {doubled}</p>
