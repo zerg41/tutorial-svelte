09e94f3aab244cdca8253ea7efc536ef5f8cb7c2
zerg41 <zerg41@gmail.com>
2023-11-30 19:48:53 +0300

    feat: show how to use derived store

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,8 @@
 <script>
+  import StoreDerived from './lib/tutorial/basic/stores/StoreDerived.svelte';
   import StoreReadable from './lib/tutorial/basic/stores/StoreReadable.svelte';
   import StoreSubscription from './lib/tutorial/basic/stores/StoreSubscription.svelte';
   import StoreWritable from './lib/tutorial/basic/stores/StoreWritable.svelte';
 </script>
 
-<StoreReadable />
+<StoreDerived />
--- src/lib/tutorial/basic/stores/stores.ts
+++ src/lib/tutorial/basic/stores/stores.ts
@@ -1,10 +1,11 @@
-import { readable, writable } from 'svelte/store';
+import { derived, readable, writable } from 'svelte/store';
 
 const ONE_SECOND = 1000;
+const startDate = new Date();
 
 export const count = writable(0);
 
-export const time = readable(new Date(), (set) => {
+export const time = readable(startDate, (set) => {
   const interval = setInterval(() => {
     set(new Date());
   }, ONE_SECOND);
@@ -13,3 +14,5 @@ export const time = readable(new Date(), (set) => {
     clearInterval(interval);
   };
 });
+
+export const elapsed = derived(time, ($time) => Math.round(($time.valueOf() - startDate.valueOf()) / ONE_SECOND));
