20e747a84e5b6041894faffe0588ec12b383a553
zerg41 <zerg41@gmail.com>
2023-11-30 19:44:23 +0300

    feat: show how to use readable store

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
+  import StoreReadable from './lib/tutorial/basic/stores/StoreReadable.svelte';
   import StoreSubscription from './lib/tutorial/basic/stores/StoreSubscription.svelte';
   import StoreWritable from './lib/tutorial/basic/stores/StoreWritable.svelte';
 </script>
 
-<StoreSubscription />
+<StoreReadable />
--- src/lib/tutorial/basic/stores/stores.ts
+++ src/lib/tutorial/basic/stores/stores.ts
@@ -1,3 +1,15 @@
-import { writable } from 'svelte/store';
+import { readable, writable } from 'svelte/store';
+
+const ONE_SECOND = 1000;
 
 export const count = writable(0);
+
+export const time = readable(new Date(), (set) => {
+  const interval = setInterval(() => {
+    set(new Date());
+  }, ONE_SECOND);
+
+  return () => {
+    clearInterval(interval);
+  };
+});
